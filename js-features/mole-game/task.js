dfe10ccf7a719e49a813900cef6c44c0e1e2846d
Sergey Stepanov <ser1653285@yandex.ru>
2021-08-23 23:25:46 +0300

    Обработчик?

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,21 +8,32 @@ const lostId = document.getElementById('lost');
 // }
 const getHole = (index) => document.getElementById(`hole${index}`);
 
-// let hit = (index) => getHole(index).classList.contains('hole_has-mole');
+let hit = (index) => getHole(index).classList.contains('hole_has-mole');
 
-getHole.onclick = function () {
-    console.log('sdfsdf');
-};
+function checkHit() {
+    if (hit) deadId.textContent++;
+    else lostId.textContent++;
 
-// if (hit) deadId.textContent++;
-// else lostId.textContent++;
+    if (deadId.textContent === 10) {
+        alert('Победа!!!');
+        deadId.textContent = 0;
+        lostId.textContent = 0;
+    } else if (lostId.textContent === 5) {
+        alert('Вы проиграли');
+        deadId.textContent = 0;
+        lostId.textContent = 0;
+    }
+}
 
-// if (deadId.textContent === 10) {
-//     alert('Победа!!!');
-//     deadId.textContent = 0;
-//     lostId.textContent = 0;
-// } else if (lostId.textContent === 5) {
-//     alert('Вы проиграли');
-//     deadId.textContent = 0;
-//     lostId.textContent = 0;
+// for (let i = 0; i <= 9; i++) {
+//     getHole[i].onclick = function () {
+//         console.log('fgfgf');
+//         // checkHit();
+//     };
 // }
+
+for (let elem of getHole) {
+    elem.onclick = function () {
+        checkHit();
+    };
+}
