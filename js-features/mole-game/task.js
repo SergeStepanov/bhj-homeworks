bf554c6515b177aea8348bf3acb57bd23edbe66d
Sergey Stepanov <ser1653285@yandex.ru>
2021-08-23 06:56:50 +0300

    ????????

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const deadId = document.getElementById('dead');
+const lostId = document.getElementById('lost');
+
+// function getHole(index) {
+//     document.getElementById(`hole${index}`);
+// }
+const getHole = (index) => document.getElementById(`hole${index}`);
+
+// let hit = (index) => getHole(index).classList.contains('hole_has-mole');
+
+getHole.onclick = function () {
+    console.log('sdfsdf');
+};
+
+// if (hit) deadId.textContent++;
+// else lostId.textContent++;
+
+// if (deadId.textContent === 10) {
+//     alert('Победа!!!');
+//     deadId.textContent = 0;
+//     lostId.textContent = 0;
+// } else if (lostId.textContent === 5) {
+//     alert('Вы проиграли');
+//     deadId.textContent = 0;
+//     lostId.textContent = 0;
+// }
