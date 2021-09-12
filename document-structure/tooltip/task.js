4ab37d72890749112fb9feac14e5ad06289a1869
Sergey <ser1653285@yandex.ru>
2021-09-12 23:43:07 +0300

    pull

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,18 +3,19 @@
 const hints = document.querySelectorAll('.has-tooltip');
 
 for (const item of hints) {
+    item.insertAdjacentHTML('afterEnd', "<div class='tooltip'></div>");
+
     item.addEventListener('click', funcHint);
 }
 
 function funcHint(e) {
     e.preventDefault();
     const hint = this.getAttribute('title');
-    const tooltipDiv = document.createElement(
-        `<div class='tooltip tooltip_active' style='left: 0; top: 0'>\`${hint}\`</div>`
-    );
-    this.insertAdjacentElement(afterEnd, tooltipDiv);
+    const tooltipDiv = document.querySelector('.tooltip');
+    tooltipDiv.textContent = hint;
 
     if (e.target) {
-        console.log(hint);
+        tooltipDiv.classList.toggle('tooltip_active');
+        console.log(tooltipDiv);
     }
 }
