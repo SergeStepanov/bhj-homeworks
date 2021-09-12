7ad721f341996966cef89cb80a17791a47844b7a
Sergey Stepanov <ser1653285@yandex.ru>
2021-09-13 01:53:51 +0300

    added todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const tasksControl = document.querySelector('.tasks__control');
+const tasksInput = document.querySelector('.tasks__input');
+const btnTasksAdd = document.querySelector('.tasks__add');
+const tasksList = document.querySelector('.tasks__list');
+
+tasksControl.addEventListener('click', addedTask);
+
+tasksControl.addEventListener('keyup', addedTask);
+
+function addedTask(e) {
+    e.preventDefault();
+
+    if (
+        (e.key === 'Enter' && tasksInput.value !== '') ||
+        (e.target === btnTasksAdd && tasksInput.value !== '')
+    ) {
+        tasksList.insertAdjacentHTML(
+            'afterBegin',
+            '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>'
+        );
+
+        const taskTitle = document.querySelector('.task__title');
+        taskTitle.textContent = tasksInput.value;
+        tasksControl.reset();
+    }
+}
+
+tasksList.addEventListener('click', deletedTask);
+
+function deletedTask(e) {
+    e.preventDefault();
+
+    const taskRemoves = document.querySelectorAll('.task__remove');
+    const task = document.querySelector('.task');
+
+    for (const item of taskRemoves) {
+        if (e.target === item) {
+            task.remove();
+        }
+    }
+}
