1316f1105e96d5571ecd08db6f16416c43b3ceec
Sergey Stepanov <ser1653285@yandex.ru>
2021-09-20 05:09:07 +0300

    fix progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -11,20 +11,10 @@ formSend.addEventListener('submit', (e) => {
 
     request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 
-    request.upload.onloadstart = function () {
-        progress.value = '0.2';
+    request.upload.onprogress = function (event) {
+        progress.value = event.loaded / event.total;
+        // console.log(progress.value);
     };
 
-    request.upload.onprogress = function () {
-        progress.value = '0.52';
-    };
-    request.upload.onload = function () {
-        progress.value = '0.75';
-    };
-
-    request.upload.onloadend = function () {
-        progress.value = '1.0';
-    };
-
-    request.send(formSend);
+    request.send(formData);
 });
